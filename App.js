58104736addf77d03ff3ea6a57a23ca515d83340
Adarsh Anand <125790093+AdarshAnand10@users.noreply.github.com>
2024-12-07 03:38:27 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormBuilder from './components/FormBuilder';
+import './styles.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <FormBuilder />
+    </div>
+  );
+};
+
+export default App;
